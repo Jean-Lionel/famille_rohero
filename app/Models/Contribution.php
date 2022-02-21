d17e8527f8c58d92ed3ce5e1081b40cd48b14737
Jean-Lionel <nijeanlionel@gmail.com>
2022-02-21 06:28:17 +0200

    display page on liste contribution

--- app/Models/Contribution.php
+++ app/Models/Contribution.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Membre;
+use App\Models\TypeCotisation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +12,17 @@ class Contribution extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function membre()
+    {
+        // code...
+        return $this->belongsTo(Membre::class);
+    }
+    public function cotisation()
+    {
+        // code...
+        return $this->belongsTo(TypeCotisation::class,'typecotisation_id');
+    }
 
     public static function boot()
     {
--- resources/views/livewire/liste-contribution.blade.php
+++ resources/views/livewire/liste-contribution.blade.php
@@ -1,3 +1,39 @@
 <div>
     {{-- Success is as dangerous as failure. --}}
+
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table" id="dataTable">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nom et Prénom</th>
+                        <th>Cellule</th>
+                        <th>Cotisation</th>
+                        <th>Montant</th>
+                        <th>Date</th>
+                    </tr>
+                    
+                </thead>
+
+                <tbody>
+                    @foreach ($contributions as $element)
+                        {{-- expr --}}
+                        <tr>
+                            <td>{{$element->id}}</td>
+                            <td>{{$element->membre->fullname ?? ""}}</td>
+                            <td>{{$element->membre->cellule->name ?? ""}}</td>
+                            <td>{{$element->cotisation->name}}</td>
+                            <td>{{number_format($element->montant)}}</td>
+                            <td>
+                                {{ $element->created_at}}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            {{ $contributions->links()}}
+        </div>
+    </div>
 </div>
