0ffcf89404afecd6c4e383d437fcf4cef2f88733
Jean-Lionel <nijeanlionel@gmail.com>
2022-02-21 09:54:53 +0200

    display

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Cellule;
+use App\Models\Compte;
+use App\Models\Membre;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard')->extends('layouts.base');
+        $data = [];
+
+        $total_membres = Membre::all()->count();
+        $montantTotal = Compte::all()->sum('montant');
+        $celluleTotal = Cellule::all()->count();
+
+        return view('livewire.dashboard',[
+            'total_membres' => $total_membres,
+            'montantTotal' => $montantTotal,
+            'celluleTotal' => $celluleTotal,
+
+        ])->extends('layouts.base');
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -8,7 +8,7 @@
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
             <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                class="fas fa-download fa-sm text-white-50"></i> Exporter le rapport</a>
             </div>
 
             <!-- Content Row -->
@@ -20,9 +20,10 @@
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Earnings (Monthly)</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1 text-center">
+                                    Montant Total</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800 text-center">
+                                    {{ number_format($montantTotal) }} FBU </div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -38,9 +39,9 @@
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                    Earnings (Annual)</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1 text-center">
+                                    Total des membres</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800 text-center">{{$total_membres}}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -56,11 +57,13 @@
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total des Cellules
                                     </div>
                                     <div class="row no-gutters align-items-center">
                                         <div class="col-auto">
-                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                                {{ $celluleTotal}}
+                                            </div>
                                         </div>
                                         <div class="col">
                                             <div class="progress progress-sm mr-2">
@@ -86,7 +89,7 @@
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                    Pending Requests</div>
+                                    Total </div>
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                                 </div>
                                 <div class="col-auto">
