cdfc545d5fe77e65f2723f131f15d0d35c04c735
Jean-Lionel <nijeanlionel@gmail.com>
2022-03-07 12:00:58 +0200

    first laravel deployment to heroku

--- database/migrations/2022_02_20_032753_create_type_cotisations_table.php
+++ database/migrations/2022_02_20_032753_create_type_cotisations_table.php
@@ -16,7 +16,6 @@ class CreateTypeCotisationsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('description')->nullable();
-            $table->string('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
             });
