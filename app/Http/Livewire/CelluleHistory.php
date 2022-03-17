bc0d9ee6f644bdc83adcbb6084388b96e8139672
Jean-Lionel <nijeanlionel@gmail.com>
2022-03-17 07:10:17 +0200

    add cellule history

---
+++ app/Http/Livewire/CelluleHistory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CelluleHistory extends Component
+{
+    public function render()
+    {
+        return view('livewire.cellule-history');
+    }
+}
